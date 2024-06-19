fa5aad138c9c30526387da4cc619ea15f2ef52c0
Jens Kornacker <jens.kornacker@phactum.at>
2024-06-19 10:08:39 +0200

    unused route removed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {TaskComponent} from "./task/task.component";
 import {WorkflowComponent} from "./workflow/workflow.component";
 import {TaskListComponent} from "./task/task-list/task-list.component";
 import {WorkflowListComponent} from "./workflow/workflow-list/workflow-list.component";
-import {NewSignalComponent} from "./course/new-signal/new-signal.component";
 import {CreateCustomerComponent} from "./customer/create-customer/create-customer.component";
 import {CustomerListComponent} from "./customer/customer-list/customer-list.component";
 import {CustomerComponent} from "./customer/customer/customer.component";
@@ -18,7 +17,6 @@ const routes: Routes = [
   {path: 'workflow-list', component: WorkflowListComponent},
   {path: 'workflow/:id', component: WorkflowComponent},
   {path: 'tasks/:id', component: TaskComponent},
-  {path: 'signals', component: NewSignalComponent},
   {path: 'create-customer', component: CreateCustomerComponent},
   {path: 'customers', component: CustomerListComponent},
   {path: 'customers/:id', component: CustomerComponent},
